d7ea5aa59131aee5e69b5fcc66dbcf4b689ff5fe
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2018-12-25 11:01:27 -0200

    Initial Spring boot configuration

---
+++ src/main/java/com/email/ApplicationConfig.java
@@ -0,0 +1,12 @@
+package com.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationConfig.class, args);
+    }
+}
